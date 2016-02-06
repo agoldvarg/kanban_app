b312681debb47f1ee9f2dc7784bf87c894ad0b13
agoldvarg <agoldvarg@gmail.com>
2016-02-06 16:30:03 -0500

    Split configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+// Requires
 const path = require('path');
+const merge = require('webpack-merge');
 
+// Target e.g. 'start'
+const TARGET = process.env.npm_lifecycle_event;
+
+// Build paths
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+// Common configuration
+const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
@@ -14,3 +21,12 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}

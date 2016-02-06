95d76dfd741ad965242e4d93ee84661c158e6001
agoldvarg <agoldvarg@gmail.com>
2016-02-06 16:53:23 -0500

    Configure hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Requires
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 // Target e.g. 'start'
 const TARGET = process.env.npm_lifecycle_event;
@@ -24,7 +25,32 @@ const common = {
 
 // Default configuration
 if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+
+  module.exports = merge(common, {
+
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based routing
+      // works. This is a good default that will come in handy in more
+      // complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amout of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT || 4444
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
 if (TARGET === 'build') {

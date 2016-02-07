89674dbc912f7db61f56a57688ebe600244f50a8
agoldvarg <agoldvarg@gmail.com>
2016-02-06 19:23:23 -0500

    Add react components

--- app/index.js
+++
@@ -1,7 +0,0 @@
-var css = require('./main.css');
-var component = require('./component');
-var app = document.createElement('div');
-
-document.body.appendChild(app);
-
-app.appendChild(component());
